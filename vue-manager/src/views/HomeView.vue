0d681f50700c917b88b1da5c837e850a23f144f5
614460810 <614460810@qq.com>
2023-02-17 19:13:41 +0800

    flex布局

--- vue-manager/src/views/HomeView.vue
+++ vue-manager/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
     <el-row :gutter=20>
         <el-col :span=8>
-            <el-card shadow="hover" class="box-card">
+            <el-card shadow="hover" class="box-card" >
                 <div class="user">
                     <img :src="userImage">
                     <div class="userInfo">
@@ -14,16 +14,33 @@
                     <p>上次登录地点:<span>福州</span></p>
                 </div>
             </el-card>
-            <el-card style="margin-top: 20px;height: 460px;">
+            <el-card style="margin-top: 20px;height: 400px;">
                 <el-table :data="tableData">
                     <!-- 遍历 -->
-                    <el-table-column v-for="(val,key) in tableLabel" :key="key" :prop="key"  :label="val"></el-table-column>
+                    <el-table-column v-for="(val, key) in tableLabel" :key="key" :prop="key" :label="val"></el-table-column>
                     <!-- <el-table-column prop="name" label="姓名"></el-table-column>
-                    <el-table-column prop="todayBuy" label="当日购买"></el-table-column>
-                    <el-table-column prop="monthBuy" label="当月购买"></el-table-column>
-                    <el-table-column prop="totalBuy" label="总购买"></el-table-column> -->
+                            <el-table-column prop="todayBuy" label="当日购买"></el-table-column>
+                            <el-table-column prop="monthBuy" label="当月购买"></el-table-column>
+                            <el-table-column prop="totalBuy" label="总购买"></el-table-column> -->
                 </el-table>
-            </el-card >
+            </el-card>
+        </el-col>
+        <el-col :span="16">
+            <div class="num">
+                <el-card v-for="item in countData" :key="item.name" :body-style="{ display: 'flex' }">
+                    <i class='icon' :class="'el-icon-' + item.icon" :style="{ background: item.color }"></i>
+                    <div class="detail">
+                        <p class="price">￥{{ item.value }}</p>
+                        <p class="des">{{ item.name }}</p>
+                    </div>
+                </el-card>
+            </div>
+            <el-card style="height: 280px;">
+                </el-card>
+            <div class="grahic">
+                <el-card style="height: 260px;"> </el-card>
+                <el-card style="height: 260px;"></el-card>
+            </div>
         </el-col>
     </el-row>
 </template>
@@ -72,13 +89,51 @@ export default {
                 }
 
             ],
-            tableLabel:{
-                name:'课程',
-                todayBuy:'当日购买',
-                monthBuy:'当月购买',
-                totalBuy:'总共购买',
+            tableLabel: {
+                name: '课程',
+                todayBuy: '当日购买',
+                monthBuy: '当月购买',
+                totalBuy: '总共购买',
 
-            }
+            },
+            countData: [
+                {
+                    name: "今日支付订单",
+                    value: 1234,
+                    icon: "success",
+                    color: "#2ec7c9",
+                },
+                {
+                    name: "今日收藏订单",
+                    value: 210,
+                    icon: "star-on",
+                    color: "#ffb980",
+                },
+                {
+                    name: "今日未支付订单",
+                    value: 1234,
+                    icon: "s-goods",
+                    color: "#5ab1ef",
+                },
+                {
+                    name: "本月支付订单",
+                    value: 1234,
+                    icon: "success",
+                    color: "#2ec7c9",
+                },
+                {
+                    name: "本月收藏订单",
+                    value: 210,
+                    icon: "star-on",
+                    color: "#ffb980",
+                },
+                {
+                    name: "本月未支付订单",
+                    value: 1234,
+                    icon: "s-goods",
+                    color: "#5ab1ef",
+                },
+            ],
         }
     }
 }
@@ -87,6 +142,7 @@ export default {
 .user {
     display: flex;
     align-items: center;
+    margin-bottom: 40px;
 
 }
 
@@ -105,4 +161,28 @@ export default {
     font-size: 22px;
     color: #999999;
     ;
-}</style>
+}
+.num{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+
+.icon {
+    width: 70px;
+    height: 70px;
+    text-align: center;
+    line-height: 80px;
+    margin-right: 10px;
+    font-size: 30px;
+}
+.grahic{
+   display: flex; 
+   justify-content: space-between;
+
+}
+.grahic .el-card {
+        width: 48%;
+    }
+
+</style>
