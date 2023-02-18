825b3e1bc98d3846befee14f990e975f136c20b9
614460810 <614460810@qq.com>
2023-02-18 15:31:53 +0800

    rules 表单校验不能为空

--- vue-manager/src/views/UserView.vue
+++ vue-manager/src/views/UserView.vue
@@ -1,30 +1,30 @@
 <template>
     <div>
         <el-dialog title="提示" :visible.sync="dialogVisible" width="50%">
-            <el-form ref="form" :model="form" label-width="80px" :inline="true">
-                <el-form-item label="姓名">
+            <el-form ref="form" :model="form" label-width="80px" :inline="true" :rules="rules">
+                <el-form-item label="姓名" prop="name">
                     <el-input v-model="form.name" placeholder="请输入姓名"></el-input>
                 </el-form-item>
-                <el-form-item label="年龄">
+                <el-form-item label="年龄" prop="age">
                     <el-input v-model="form.age" placeholder="请输入年龄"></el-input>
                 </el-form-item>
-                <el-form-item label="性别">
+                <el-form-item label="性别" prop="sex">
                     <el-select v-model="form.sex" placeholder="请选择">
                         <el-option label="男" value="1"></el-option>
                         <el-option label="女" value="0"></el-option>
                     </el-select>
                 </el-form-item>
-                <el-form-item label="出生日期">
+                <el-form-item label="出生日期" prop="birth">
                     <el-date-picker v-model="form.birth" type="date" placeholder="选择日期">
                     </el-date-picker>
                 </el-form-item>
-                <el-form-item label="地址">
+                <el-form-item label="地址" prop="addr">
                     <el-input v-model="form.addr" placeholder="请输入地址"></el-input>
                 </el-form-item>
             </el-form>
             <span slot="footer" class="dialog-footer">
                 <el-button @click="dialogVisible = false">取 消</el-button>
-                <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+                <el-button type="primary" @click="submit">确 定</el-button>
             </span>
         </el-dialog>
         <el-button type="primary" @click="dialogVisible = true">+ 新增用户</el-button>
@@ -43,8 +43,37 @@ export default {
                 birth: '',
                 addr: '',
 
+            },
+            // ：rules="rules" 表单校验 不能为空
+            rules:{
+                name:[{
+                    required:true,
+                    message:'请输入名字'
+                }],
+                age:[{
+                    required:true,
+                    message:'请输入年纪'
+                }],
+                sex:[{
+                    required:true,
+                    message:'请选择性别'
+                }],
+                birth:[{
+                    required:true,
+                    message:'请输入生日'
+                }],
+                addr:[{
+                    required:true,
+                    message:'请输入地址'
+                }],
+
             }
         };
+    },
+    methods:{
+        submit(){
+
+        }
     }
 }
 </script>
