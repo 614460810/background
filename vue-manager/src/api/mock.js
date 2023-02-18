4e7ec111234c6d02b5e8bae429da00d1583382f4
614460810 <614460810@qq.com>
2023-02-18 16:49:49 +0800

    mock

--- vue-manager/src/api/mock.js
+++ vue-manager/src/api/mock.js
@@ -1,3 +1,10 @@
 import Mock from "mockjs";
 import homeApi from './mockServeData/home'
+import user from './mockServeData/user'
+
+// mock拦截
 Mock.mock('/api/home/getData',homeApi.getStatisticalData)
+Mock.mock('/api/user/add', 'post', user.createUser)
+Mock.mock('/api/user/edit', 'post', user.updateUser)
+Mock.mock('/api/user/del', 'post', user.deleteUser)
+Mock.mock('/api/user/getUser/', user.getUser)
--- vue-manager/src/views/UserView.vue
+++ vue-manager/src/views/UserView.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <el-dialog title="提示" :visible.sync="dialogVisible" width="50%"  :before-close="handleClose">
+        <el-dialog title="提示" :visible.sync="dialogVisible" width="50%" :before-close="handleClose">
             <el-form ref="form" :model="form" label-width="80px" :inline="true" :rules="rules">
                 <el-form-item label="姓名" prop="name">
                     <el-input v-model="form.name" placeholder="请输入姓名"></el-input>
@@ -28,9 +28,19 @@
             </span>
         </el-dialog>
         <el-button type="primary" @click="dialogVisible = true">+ 新增用户</el-button>
+        <!-- 表格 -->
+        <el-table :data="tableData" style="width: 100%">
+            <el-table-column prop="date" label="日期" width="180">
+            </el-table-column>
+            <el-table-column prop="name" label="姓名" width="180">
+            </el-table-column>
+            <el-table-column prop="address" label="地址">
+            </el-table-column>
+        </el-table>
     </div>
 </template>
 <script>
+import { getUser } from '../api'
 export default {
     name: 'UserView',
     data() {
@@ -45,52 +55,60 @@ export default {
 
             },
             // ：rules="rules" 表单校验 不能为空
-            rules:{
-                name:[{
-                    required:true,
-                    message:'请输入名字'
+            rules: {
+                name: [{
+                    required: true,
+                    message: '请输入名字'
                 }],
-                age:[{
-                    required:true,
-                    message:'请输入年纪'
+                age: [{
+                    required: true,
+                    message: '请输入年纪'
                 }],
-                sex:[{
-                    required:true,
-                    message:'请选择性别'
+                sex: [{
+                    required: true,
+                    message: '请选择性别'
                 }],
-                birth:[{
-                    required:true,
-                    message:'请输入生日'
+                birth: [{
+                    required: true,
+                    message: '请输入生日'
                 }],
-                addr:[{
-                    required:true,
-                    message:'请输入地址'
+                addr: [{
+                    required: true,
+                    message: '请输入地址'
                 }],
 
-            }
+            },
+            tableData:[]
         };
     },
-    methods:{
-        submit(){
+    methods: {
+        submit() {
             // 表单校验 返回一个值true false
-            this.$refs.form.validate(val=>{
+            this.$refs.form.validate(val => {
                 console.log(val)
-                if(val){
+                if (val) {
                     console.log(this.form)
-                    关闭表单
-                    this.dialogVisible=false
+                    // 关闭表单
+                    this.dialogVisible = false
                 }
             })
 
         },
-        handleClose(){
-            重置表单内容
+        handleClose() {
+            // 重置表单内容
             this.$refs.form.resetFields()
-            this.dialogVisible=false
+            this.dialogVisible = false
         },
-        cancel(){
+        cancel() {
             this.handleClose()
         }
+    },
+    mounted(){
+        // 获取列表数据
+        getUser().then(({ data }) => {
+            
+            console.log(data)
+        })
     }
 }
 </script>
